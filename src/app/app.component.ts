d1c5d7ba169f8a8225e83768cb64fb2ebeee2363
soniikot <soniikot@gmail.com>
2025-04-07 08:04:23 -0700

    start  the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NgForm} from '@angular/forms';
+import {Guid} from 'guid-typescript';
+import {Todo} from "../models/todo.model";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'todo-app';
+todoList: Todo []  =[]
+
+onSubmit  (form:NgForm){
+  const todo = new Todo (Guid.create(),   form.value.title,   false);
+  this.todoList.push(todo);
+  form.resetForm();
+}
+
+onComplete (id:Guid){
+  const todo = this.todoList.find(item=>item.id ===id);
+  todo ? todo.isDone = true : console.error("Something wrong") ;
+}
+
+onDelete(id: Guid) {
+  const index = this.todoList.findIndex(item => item.id === id);
+  if (index > -1) {
+    this.todoList.splice(index, 1);
+  }
+}
 }
